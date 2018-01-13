a285f744082aff8403f8463ed6285f01249ff90a
zsmith27 <zsmith@icprb.org>
2018-01-12 20:34:51 -0500

    Updated functions related to calculating metric sensitivity.

--- R/metric_sensitivity.R
+++ R/metric_sensitivity.R
@@ -1,109 +1,335 @@
 #==============================================================================
 #Metric Sensitivity
 #==============================================================================
-#'Metric Sensitivity
-#'
-#'@param metrics.df = data frame of metric values for each station
-#'@param ref.cond = The site classification that represents better
-#'environmental conditions.
-#'@param deg.cond = The site classification that represents the degraded
-#'environmental conditions.
-#'@param method = the sensitivity function to be used during the assessment.
-#'@return Determines the threshold at which a metric best categorizes
-#'reference and degraded stations.
-#'@export
-#'
-sensitivity <- function(metrics.df, first.metric, condition.colname,
-                        ref.cond = "REF", deg.cond = "DEG",
-                        method = "BDE"){
- 
-  #Create new data frames specific for Degraded and Reference sites
-  deg.df <- metrics.df[metrics.df[, condition.colname] %in% deg.cond, ]
-  ref.df <- metrics.df[metrics.df[, condition.colname] %in% ref.cond, ]
-  #============================================================================
-  #Calculate the median values for the reference and degraded distributions.
-  metric_col.1 <- which(names(metrics.df) %in% first.metric)
-  if(ncol(metrics.df) == metric_col.1){
-    ref_50 <- quantile(ref.df[, metric_col.1], 0.50, na.rm = TRUE)
-    deg_50 <- quantile(deg.df[, metric_col.1], 0.50, na.rm = TRUE)
-    
-    #Provide the each reference percentile value for each metric.
-    quant.ref <- data.frame(quantile(ref.df[, metric_col.1], probs = seq(0, 1, by = 0.01), na.rm = TRUE))
-    colnames(quant.ref) <- colnames(ref.df)[metric_col.1]
-    #Create a column listing all of the metrics and join the reference percentile values
-    quant.df <- cbind(data.frame(colnames(metrics.df[metric_col.1])), t(quant.ref))
-    names(quant.df)[1] <- "METRICS" #Rename column 1
-    
-  
-    #Create new data frames specific for Degraded and Reference sites
-    severe.df <- metrics.df[metrics.df[, condition.colname] %in% deg.cond, ]
-    reference.df <- metrics.df[metrics.df[, condition.colname] %in% ref.cond, ]
-    #Calculate the median values for the reference and degraded distributions.
-    reference_50 <- quantile(reference.df[, metric_col.1],  0.50, na.rm = TRUE)
-    severe_50 <- quantile(severe.df[, metric_col.1],  0.50, na.rm = TRUE)
-    #Insert a column to suggest how the metric reacts to disturbance. If the reference median
-    # is greater than the degraded median, the metric decreases with distrubance. If the reference
-    # median is less than the degraded median, the metric increases with disturbance.  If the
-    # medians are equal, equal is return to indicate that this metric shows no distinction between
-    # reference and degraded contions.
-    quant.df$DISTURBANCE <- ifelse(reference_50 > severe_50, "DECREASE",
-                                   ifelse(reference_50 < severe_50, "INCREASE", "EQUAL"))
-    
-  }
-  
-  if(length(ref.df) > metric_col.1){
-    ref_50 <- sapply(ref.df[, metric_col.1:ncol(ref.df)], quantile, 0.50, na.rm = TRUE)
-    deg_50 <- sapply(deg.df[, metric_col.1:ncol(deg.df)], quantile, 0.50, na.rm = TRUE)
-    
-    #Provide the each reference percentile value for each metric.
-    quant.ref <- data.frame(apply(ref.df[, metric_col.1:ncol(ref.df)], 2, function(x) quantile(x, probs = seq(0, 1, by = 0.01), na.rm = TRUE)))
-    #Create a column listing all of the metrics and join the reference percentile values
-    quant.df <- cbind(data.frame(colnames(metrics.df[metric_col.1:ncol(metrics.df)])), t(quant.ref))
-    names(quant.df)[1] <- "METRICS" #Rename column 1
+#' @title Spread Condition
+#' @description FUNCTION_DESCRIPTION
+#' @param x PARAM_DESCRIPTION
+#' @param metric.col PARAM_DESCRIPTION
+#' @param condition.col PARAM_DESCRIPTION
+#' @param value.col PARAM_DESCRIPTION
+#' @param ref.cond PARAM_DESCRIPTION
+#' @param deg.cond PARAM_DESCRIPTION
+#' @return OUTPUT_DESCRIPTION
+#' @details DETAILS
+#' @examples
+#' \dontrun{
+#' if(interactive()){
+#'  #EXAMPLE1
+#'  }
+#' }
+#' @seealso
+#'  \code{\link[rlang]{enquo}},\code{\link[rlang]{sym}},\code{\link[rlang]{quo_name}}
+#' @rdname spread_condition
+#' @export
+#' @importFrom rlang enquo sym quo_name
+#' @importFrom magrittr "%>%"
 
-    
-    #Create new data frames specific for Degraded and Reference sites
-    severe.df <- metrics.df[metrics.df[, condition.colname] %in% deg.cond, ]
-    reference.df <- metrics.df[metrics.df[, condition.colname] %in% ref.cond, ]
-    #Calculate the median values for the reference and degraded distributions.
-    reference_50 <- sapply(reference.df[, metric_col.1:ncol(reference.df)], quantile, 0.50, na.rm = TRUE)
-    severe_50 <- sapply(severe.df[, metric_col.1:ncol(severe.df)], quantile, 0.50, na.rm = TRUE)
-    #Insert a column to suggest how the metric reacts to disturbance. If the reference median
-    # is greater than the degraded median, the metric decreases with distrubance. If the reference
-    # median is less than the degraded median, the metric increases with disturbance.  If the
-    # medians are equal, equal is return to indicate that this metric shows no distinction between
-    # reference and degraded contions.
-    quant.df$DISTURBANCE <- ifelse(reference_50 > severe_50, "DECREASE",
-                                   ifelse(reference_50 < severe_50, "INCREASE", "EQUAL"))
-  }
-  
-  #============================================================================
-  if(method %in% c("BARBOUR", "ALL")){
-    barb.df <- barbour(metrics.df, first.metric, ref.df, deg.df)
-  }
-  
-  if(method %in% c("DE", "ALL")){
-    de.df <- original_de(deg.df, quant.df, first.metric)
-  }
-  
-  if(method %in% c("BDE", "ALL")){
-    bde.df <- balanced_ce(metrics.df, first.metric, quant.df,
-                            condition.colname,
-                            ref.cond, deg.cond,
-                            ref.df, deg.df, quant.ref)
-  }
-  
-  if(method %in% "ALL"){
-    names(barb.df)[names(barb.df) %in% "SENSITIVITY"] <- "BARBOUR_SENSITIVITY"
-    names(de.df)[names(de.df) %in% "SENSITIVITY"] <- "DE_SENSITIVITY"
-    names(bde.df)[names(bde.df) %in% "SENSITIVITY"] <- "BDE_SENSITIVITY"
-    final.df <- plyr::join_all(list(barb.df, de.df, bde.df), c("METRICS", "DISTURBANCE"))
-  } else {
-    if(exists("barb.df")) final.df <- barb.df
-    if(exists("de.df")) final.df <- de.df
-    if(exists("bde.df")) final.df <- bde.df
-  }
-  
-  
+spread_condition  <- function(x, metric.col, condition.col, value.col, ref.cond, deg.cond) {
+  metric.col <- rlang::enquo(metric.col)
+  value.col <- rlang::enquo(value.col)
+  condition.col <- rlang::enquo(condition.col)
+  ref.col.name <- rlang::sym(paste("ref", rlang::quo_name(value.col), sep = "_"))
+  deg.col.name <- rlang::sym(paste("deg", rlang::quo_name(value.col), sep = "_"))
+
+  x %>%
+    select(!!metric.col, !!condition.col, !!value.col) %>%
+    spread(!!condition.col, !!value.col) %>%
+    rename(!!ref.col.name := !!rlang::sym(ref.cond),
+           !!deg.col.name := !!rlang::sym(deg.cond))
+}
+
+#==============================================================================
+#' @title Prepare Sensitivity
+#' @description FUNCTION_DESCRIPTION
+#' @param metrics.long PARAM_DESCRIPTION
+#' @param metric.col PARAM_DESCRIPTION
+#' @param value.col PARAM_DESCRIPTION
+#' @param condition.col PARAM_DESCRIPTION
+#' @param ref.cond PARAM_DESCRIPTION
+#' @param deg.cond PARAM_DESCRIPTION
+#' @return OUTPUT_DESCRIPTION
+#' @details DETAILS
+#' @examples
+#' \dontrun{
+#' if(interactive()){
+#'  #EXAMPLE1
+#'  }
+#' }
+#' @seealso
+#'  \code{\link[rlang]{enquo}},\code{\link[rlang]{quo_name}}
+#' @rdname prep_sensitivity
+#' @export
+#' @importFrom rlang enquo quo_name
+
+prep_sensitivity <- function(metrics.long, metric.col, value.col, condition.col, ref.cond, deg.cond) {
+  metric.col <- rlang::enquo(metric.col)
+  value.col <- rlang::enquo(value.col)
+  condition.col <- rlang::enquo(condition.col)
+
+  metrics.df <- metrics.long %>%
+    select(!!metric.col, !!value.col, !!condition.col) %>%
+    filter((!!condition.col) %in% c(ref.cond, deg.cond))
+
+  metrics.df <- metrics.df %>%
+    group_by(!!metric.col, !!condition.col) %>%
+    summarize(values = list(!!value.col),
+              total = length(!!value.col),
+              quantiles = list(quantile(!!value.col, probs = seq(0, 1, by = 0.01), na.rm = TRUE)),
+              quant25 = quantile(!!value.col, 0.25, na.rm = TRUE),
+              median = quantile(!!value.col, 0.50, na.rm = TRUE),
+              quant75 = quantile(!!value.col, 0.75, na.rm = TRUE)) %>%
+    ungroup()
+
+
+  final.df <- left_join(
+    spread_condition(metrics.df, !!metric.col, !!condition.col, values, ref.cond, deg.cond),
+    spread_condition(metrics.df, !!metric.col, !!condition.col, total, ref.cond, deg.cond),
+    by = rlang::quo_name(metric.col)) %>%
+    left_join(spread_condition(metrics.df, !!metric.col, !!condition.col, median, ref.cond, deg.cond),
+              by = rlang::quo_name(metric.col)) %>%
+    left_join(spread_condition(metrics.df, !!metric.col, !!condition.col, quant25, ref.cond, deg.cond),
+              by = rlang::quo_name(metric.col)) %>%
+    left_join(spread_condition(metrics.df, !!metric.col, !!condition.col, quant75, ref.cond, deg.cond),
+              by = rlang::quo_name(metric.col)) %>%
+    left_join(spread_condition(metrics.df, !!metric.col, !!condition.col, quantiles, ref.cond, deg.cond),
+              by = rlang::quo_name(metric.col))
+
+  final.df <- final.df %>%
+    mutate(disturbance = case_when(
+      ref_median > deg_median ~ "decrease",
+      ref_median < deg_median ~ "increase",
+      ref_median == deg_median ~ "equal",
+      TRUE ~ "ERROR"
+    ),
+    total = ref_total + deg_total)
+
+  return(final.df)
+}
+#==============================================================================
+#' @title Barbour et. al 1996
+#' @description FUNCTION_DESCRIPTION
+#' @param prep.sensitivity PARAM_DESCRIPTION
+#' @param metric.col PARAM_DESCRIPTION
+#' @return OUTPUT_DESCRIPTION
+#' @details DETAILS
+#' @examples
+#' \dontrun{
+#' if(interactive()){
+#'  #EXAMPLE1
+#'  }
+#' }
+#' @seealso
+#'  \code{\link[rlang]{enquo}}
+#' @rdname barbour
+#' @export
+#' @importFrom rlang enquo
+
+barbour <- function(prep.sensitivity, metric.col) {
+  metric.col <- rlang::enquo(metric.col)
+
+  prep.sensitivity %>%
+    mutate(barbour = case_when(
+      ref_median <= deg_quant75 & ref_median >= deg_quant25 &
+        deg_median <= ref_quant75 & deg_median >= ref_quant25 ~ 0,
+      ref_median <= deg_quant75 & ref_median >= deg_quant25  ~ 1,
+      deg_median <= ref_quant75 & deg_median >= ref_quant25 ~ 1,
+      ref_quant25 <= deg_quant75 & ref_quant25 >= deg_quant25 ~ 2,
+      ref_quant75 <= deg_quant75 & ref_quant75 >= deg_quant25 ~ 2,
+      TRUE ~ 3)) %>%
+    select(!!metric.col, barbour)
+}
+#==============================================================================
+#' @title Discrimination Efficiency
+#' @description FUNCTION_DESCRIPTION
+#' @param prep.sensitivity PARAM_DESCRIPTION
+#' @param metric.col PARAM_DESCRIPTION
+#' @return OUTPUT_DESCRIPTION
+#' @details DETAILS
+#' @examples
+#' \dontrun{
+#' if(interactive()){
+#'  #EXAMPLE1
+#'  }
+#' }
+#' @seealso
+#'  \code{\link[rlang]{enquo}}
+#' @rdname de_orginal
+#' @export
+#' @importFrom rlang enquo
+
+de_orginal <- function(prep.sensitivity, metric.col) {
+  metric.col <- rlang::enquo(metric.col)
+
+  prep.sensitivity %>%
+    group_by(!!metric.col) %>%
+    mutate(de = case_when(
+      disturbance == "decrease" ~ sum((unlist(deg_values) < ref_quant25)) / deg_total * 100,
+      disturbance == "equal" ~ as.double(0),
+      disturbance == "increase"  ~ sum(unlist(deg_values) > ref_quant75) / deg_total * 100,
+      TRUE ~ as.double(10 ^ 6)
+    ),
+    de_thresh = case_when(
+      disturbance == "decrease" ~ ref_quant25,
+      disturbance == "equal" ~ as.double(0),
+      disturbance == "increase"  ~ ref_quant75,
+      TRUE ~ as.double(10 ^ 6)
+    )) %>%
+    ungroup() %>%
+    select(!!metric.col, de_thresh, de)
+}
+#==============================================================================
+#' @title Test Sensitivity Thresholds
+#' @description FUNCTION_DESCRIPTION
+#' @param x PARAM_DESCRIPTION
+#' @param less.than.col PARAM_DESCRIPTION
+#' @param greater.than.col PARAM_DESCRIPTION
+#' @param thresh PARAM_DESCRIPTION
+#' @return OUTPUT_DESCRIPTION
+#' @details DETAILS
+#' @examples
+#' \dontrun{
+#' if(interactive()){
+#'  #EXAMPLE1
+#'  }
+#' }
+#' @seealso
+#'  \code{\link[rlang]{enquo}}
+#' @rdname test_thresh
+#' @export
+#' @importFrom rlang enquo
+
+test_thresh <- function(x, less.than.col, greater.than.col, thresh) {
+  less.than.col <- rlang::enquo(less.than.col)
+  greater.than.col <- rlang::enquo(greater.than.col)
+
+  final.df <- x %>%
+    mutate(group1_pct = sum((unlist(!!less.than.col) < thresh)) / length(unlist(!!less.than.col)) * 100,
+           group2_pct = sum((unlist(!!greater.than.col) > thresh)) / length(unlist(!!greater.than.col)) * 100,
+           bal_factor = abs(group1_pct - group2_pct),
+           bde = (sum(group1_pct, group2_pct) / 2),
+           bde_bf = bde - bal_factor,
+           bde_thresh = thresh) %>%
+    ungroup() #%>%
+  #select(bde_thresh, bde)
+
+  list(final.df$bde_thresh, final.df$bde_bf, final.df$bde)
+}
+#==============================================================================
+#' @title Select A Sensitivity Threshold
+#' @description FUNCTION_DESCRIPTION
+#' @param x PARAM_DESCRIPTION
+#' @param thresh.col PARAM_DESCRIPTION
+#' @return OUTPUT_DESCRIPTION
+#' @details DETAILS
+#' @examples
+#' \dontrun{
+#' if(interactive()){
+#'  #EXAMPLE1
+#'  }
+#' }
+#' @seealso
+#'  \code{\link[rlang]{enquo}}
+#'  \code{\link[purrr]{map}}
+#' @rdname select_thresh
+#' @export
+#' @importFrom rlang enquo
+#' @importFrom purrr map
+
+select_thresh <- function(x, thresh.col) {
+  thresh.col <- rlang::enquo(thresh.col)
+  test4 <- x %>%
+    #head(1) %>%
+    mutate(bde_list = list(purrr::map(unlist(!!thresh.col), function(thresh.i) {
+      if (disturbance == "decrease") {
+        test_thresh(x = ., less.than.col = deg_values, greater.than.col = ref_values, thresh = thresh.i)
+      } else if(disturbance == "increase") {
+        test_thresh(x = ., less.than.col = ref_values, greater.than.col = deg_values, thresh = thresh.i)
+      } else if(disturbance == "equal") list(ref_median, 50.00, 50.00)
+    }))) %>%
+    mutate(bde_vec = list(map_dbl(bde_list[1][[1]],`[[`, 2))) %>%
+    mutate(sel_vec = bde_list[1][[1]][median(which(unlist(bde_vec) == max(unlist(bde_vec))))])
+
+  test4$sel_vec
+}
+
+#==============================================================================
+#' @title BDE
+#' @description FUNCTION_DESCRIPTION
+#' @param prep.sensitivity PARAM_DESCRIPTION
+#' @param metric.col PARAM_DESCRIPTION
+#' @return OUTPUT_DESCRIPTION
+#' @details DETAILS
+#' @examples
+#' \dontrun{
+#' if(interactive()){
+#'  #EXAMPLE1
+#'  }
+#' }
+#' @seealso
+#'  \code{\link[rlang]{enquo}}
+#'  \code{\link[purrr]{map}}
+#' @rdname bde
+#' @export
+#' @importFrom rlang enquo
+#' @importFrom purrr map
+
+bde <- function(prep.sensitivity, metric.col) {
+  metric.col <- rlang::enquo(metric.col)
+
+  prep.sensitivity %>%
+    group_by(!!metric.col) %>%
+    mutate(pthresh = list(seq(ref_median, deg_median,
+                              by = -1* ((ref_median - deg_median) / (25))))) %>%
+    ungroup() %>%
+    mutate(sel_vec = purrr::map(seq(nrow(.)), function(row.i) {
+      slice(., row.i) %>%
+        select_thresh(pthresh)
+    })) %>%
+    rowwise() %>%
+    mutate(bde_thresh = unlist(sel_vec)[1],
+           bde = unlist(sel_vec)[3]) %>%
+    ungroup() %>%
+    select(!!metric.col, bde_thresh, bde)
+}
+#==============================================================================
+#' @title Metric Sensitivity
+#' @description FUNCTION_DESCRIPTION
+#' @param metrics.long PARAM_DESCRIPTION
+#' @param metric.col PARAM_DESCRIPTION
+#' @param value.col PARAM_DESCRIPTION
+#' @param condition.col PARAM_DESCRIPTION
+#' @param ref.cond PARAM_DESCRIPTION
+#' @param deg.cond PARAM_DESCRIPTION
+#' @return OUTPUT_DESCRIPTION
+#' @details DETAILS
+#' @examples
+#' \dontrun{
+#' if(interactive()){
+#'  #EXAMPLE1
+#'  }
+#' }
+#' @seealso
+#'  \code{\link[rlang]{enquo}},\code{\link[rlang]{quo_name}}
+#' @rdname sensitivity
+#' @export
+#' @importFrom rlang enquo quo_name
+
+sensitivity <- function(metrics.long, metric.col, value.col,
+                        condition.col, ref.cond, deg.cond) {
+  metric.col <- rlang::enquo(metric.col)
+  value.col <- rlang::enquo(value.col)
+  condition.col <- rlang::enquo(condition.col)
+
+  prep.df <- prep_sensitivity(metrics.long, !!metric.col, !!value.col, !!condition.col, ref.cond, deg.cond)
+
+  final.df <- left_join(select(prep.df, !!metric.col, disturbance),
+                        barbour(prep.df, !!metric.col),
+                        by = rlang::quo_name(metric.col)) %>%
+    left_join(de_orginal(prep.df, !!metric.col),
+              by = rlang::quo_name(metric.col)) %>%
+    left_join(bde(prep.df, !!metric.col),
+              by = rlang::quo_name(metric.col))
+
+  final.df <- arrange(final.df, desc(bde))
   return(final.df)
 }
