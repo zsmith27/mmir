98c254ca9ba96bc099b94a328b9782288a0c01f8
zsmith27 <zsmith@icprb.org>
2018-07-04 12:28:23 -0400

    keep.na is obsolete.

--- R/sequence.R
+++ R/sequence.R
@@ -63,8 +63,7 @@ taxa_seq <- function(long.df, unique.id.col, count.col, taxa.cols,
                             unique.id.col = rlang::UQ(u.col),
                             count.col = rlang::UQ(c.col),
                             taxon.col = rlang::UQ(rlang::sym(col.i)),
-                            taxon = taxa.i,
-                            keep.na = kn)
+                            taxon = taxa.i)
       }
       #------------------------------------------------------------------------
       if (job %in% c("shannon", "effective_shannon", "simpson", "invsimpson",
