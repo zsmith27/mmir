4ca67601019c985d2c40f4e1fc574ccbd4f0ba83
Smith <zachary.smith1@dec.ny.gov>
2019-02-06 14:53:15 -0500

    commented out to allow for build.

--- R/redundancy.R
+++ R/redundancy.R
@@ -1,13 +1,13 @@
-metrics.vec <- sensitivity.df %>%
-  filter(de >= 70) %>%
-  pull(metric)
-
-metric.sub <- metrics.wide %>%
-  select(metrics.vec)
-
-cor.df <- data.frame(cor(metric.sub), method = "spearman")
-cor.df$metric_y <- row.names(cor.df)
-
-long.df <- cor.df %>%
-  gather(metric_x, value, - metric_y) %>%
-  distinct()
+# metrics.vec <- sensitivity.df %>%
+#   filter(de >= 70) %>%
+#   pull(metric)
+#
+# metric.sub <- metrics.wide %>%
+#   select(metrics.vec)
+#
+# cor.df <- data.frame(cor(metric.sub), method = "spearman")
+# cor.df$metric_y <- row.names(cor.df)
+#
+# long.df <- cor.df %>%
+#   gather(metric_x, value, - metric_y) %>%
+#   distinct()
