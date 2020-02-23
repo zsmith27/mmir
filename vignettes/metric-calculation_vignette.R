fd71aae01b02d70cd937bfe416de90bf0108acb4
Zachary M. Smith <zsmith2727@gmail.com>
2020-02-23 11:08:00 -0500

    Updated the vignette to use kable instead of DT::datatable to drastically reduce the size of the vignette and ultimatley the size of the package.

--- vignettes/metric-calculation_vignette.R
+++ vignettes/metric-calculation_vignette.R
@@ -4,24 +4,25 @@ knitr::opts_chunk$set(
   comment = "#>"
 )
 
-## ---- eval=FALSE--------------------------------------------------------------
-#  install.packages("devtools", "DT")
+## ----install_helper_packages, eval=FALSE--------------------------------------
+#  install.packages("devtools",
+#                   "dplyr")
 
-## ---- eval=FALSE--------------------------------------------------------------
+## ----install_mmir, eval=FALSE-------------------------------------------------
 #  devtools::install_github("zsmith27/mmir")
 
-## -----------------------------------------------------------------------------
+## ----load_mmir----------------------------------------------------------------
 library(mmir)
 
-## -----------------------------------------------------------------------------
+## ----load_data----------------------------------------------------------------
 data("nrsa_nap_0809", package = "mmir")
 
-## -----------------------------------------------------------------------------
-metrics.df <- nrsa_nap_0809 %>% 
-  dplyr::group_nest(uid, .key = "data") 
+## ----group_nest---------------------------------------------------------------
+nest.df <- nrsa_nap_0809 %>% 
+  dplyr::group_nest(uid,  rt_nrsa_cat, .key = "data") 
 
-## -----------------------------------------------------------------------------
-metrics.df <- metrics.df %>% 
+## ----rich---------------------------------------------------------------------
+rich.df <- nest.df %>% 
   dplyr::mutate(
     rich_family = taxa_rich(.dataframe = .,
                             .key_col = uid,
@@ -34,10 +35,13 @@ metrics.df <- metrics.df %>%
   )
 
 
-DT::datatable(metrics.df, options = list(scrollX = TRUE))  
+rich.df %>% 
+  dplyr::mutate(data = "nested dataframe") %>% 
+  head() %>% 
+  knitr::kable()
 
-## -----------------------------------------------------------------------------
-metrics.df <- metrics.df %>% 
+## ----sub_rich-----------------------------------------------------------------
+sub_rich.df <- nest.df %>% 
   dplyr::mutate(
     rich_ephemeroptera_fam = taxa_rich(.dataframe = .,
                             .key_col = uid,
@@ -58,10 +62,13 @@ metrics.df <- metrics.df %>%
                             .unnest_col = data)
   )
 
-DT::datatable(metrics.df, options = list(scrollX = TRUE))  
+sub_rich.df %>% 
+  dplyr::mutate(data = "nested dataframe") %>% 
+  head() %>% 
+  knitr::kable() 
 
-## -----------------------------------------------------------------------------
-metrics.df <- metrics.df %>% 
+## ----div----------------------------------------------------------------------
+div.df <- nest.df %>% 
   dplyr::mutate(
     shannon_genus = taxa_div(.dataframe = .,
                             .key_col = uid,
@@ -96,10 +103,13 @@ metrics.df <- metrics.df %>%
                             .unnest_col = data)
   )
 
-DT::datatable(metrics.df, options = list(scrollX = TRUE))  
+div.df %>% 
+  dplyr::mutate(data = "nested dataframe") %>% 
+  head() %>% 
+  knitr::kable() 
 
-## ---- eval=FALSE--------------------------------------------------------------
-#  metrics.df <- metrics.df %>%
+## ----sub_div, eval=FALSE------------------------------------------------------
+#  sub_div.df <- nest.df %>%
 #    dplyr::mutate(
 #      gini_simpson_ept = taxa_div(.dataframe = .,
 #                              .key_col = uid,
@@ -131,10 +141,35 @@ DT::datatable(metrics.df, options = list(scrollX = TRUE))
 #                              .unnest_col = data)
 #    )
 #  
-#  DT::datatable(subdiv.df, options = list(scrollX = TRUE))
+#  sub_div.df %>%
+#    dplyr::mutate(data = "nested dataframe") %>%
+#    head() %>%
+#    knitr::kable()
 
 ## -----------------------------------------------------------------------------
-metrics.df <- metrics.df %>% 
+dom.df <- nest.df %>% 
+  dplyr::mutate(
+    dom_1_target_taxon = taxa_dom(.dataframe = .,
+                                  .key_col = uid,
+                                  .counts_col = total,
+                                  .group_col = target_taxon,
+                                  .dom_level = 1,
+                                  .unnest_col = data),
+    dom_5_target_taxon = taxa_dom(.dataframe = .,
+                                  .key_col = uid,
+                                  .counts_col = total,
+                                  .group_col = target_taxon,
+                                  .dom_level = 5,
+                                  .unnest_col = data)
+  )
+
+dom.df %>% 
+  dplyr::mutate(data = "nested dataframe") %>% 
+  head() %>% 
+  knitr::kable() 
+
+## ----pct----------------------------------------------------------------------
+pct.df <- nest.df %>% 
   dplyr::mutate(
     pct_ephemeroptera = taxa_pct(.dataframe = .,
                                   .key_col = uid,
@@ -150,10 +185,13 @@ metrics.df <- metrics.df %>%
                        .unnest_col = data)
   )
 
-DT::datatable(metrics.df, options = list(scrollX = TRUE))  
+pct.df %>% 
+  dplyr::mutate(data = "nested dataframe") %>% 
+  head() %>% 
+  knitr::kable()
 
-## -----------------------------------------------------------------------------
-metrics.df <- metrics.df %>% 
+## ----abund--------------------------------------------------------------------
+abund.df <- nest.df %>% 
   dplyr::mutate(
     abund_ephemeroptera = taxa_abund(.,
                                   .key_col = uid,
@@ -169,40 +207,137 @@ metrics.df <- metrics.df %>%
                         .unnest_col = data)
   )
 
-DT::datatable(metrics.df, options = list(scrollX = TRUE))  
+abund.df %>% 
+  dplyr::mutate(data = "nested dataframe") %>% 
+  head() %>% 
+  knitr::kable()
 
-## -----------------------------------------------------------------------------
-metrics.df <- metrics.df %>% 
+## ----seq----------------------------------------------------------------------
+seq.df <- nest.df %>% 
   dplyr::bind_cols(
     taxa_seq(.dataframe = .,
              .key_col = uid,
              .counts_col = total,
-             .filter_cols_vec = c("class", "order", "family"),
+             .filter_cols_vec = c("class", "order"),
              .group_col = genus,
              .job = "rich",
-             .unnest_col = data),
-    taxa_seq(.dataframe = .,
-             .key_col = uid,
-             .counts_col = total,
-             .filter_cols_vec = c("class", "order", "family"),
-             .group_col = genus,
-             .job = "pct_rich",
-             .unnest_col = data),
-    taxa_seq(.dataframe = .,
-             .key_col = uid,
-             .counts_col = total,
-             .filter_cols_vec = c("class", "order", "family"),
-             .group_col = genus,
-             .job = "pct",
-             .unnest_col = data),
-    taxa_seq(.dataframe = .,
-             .key_col = uid,
-             .counts_col = total,
-             .filter_cols_vec = c("class", "order", "family"),
-             .group_col = genus,
-             .base_log = 2,
-             .job = "shannon",
-             .unnest_col = data))
+             .unnest_col = data)
+    )
 
-DT::datatable(metrics.df, options = list(scrollX = TRUE))  
+dplyr::tibble(
+  "List of Column Names" = names(seq.df)
+) %>% 
+knitr::kable()
+
+seq.df %>% 
+  dplyr::mutate(data = "nested dataframe") %>% 
+  head() %>% 
+  dplyr::select(uid:rich_genus_bivalvia) %>% 
+  knitr::kable()
+
+## ----putting_it_together, eval=FALSE------------------------------------------
+#  metrics.df <- nest.df %>%
+#    dplyr::mutate(
+#      rich_family = taxa_rich(.dataframe = .,
+#                              .key_col = uid,
+#                              .group_col = family,
+#                              .unnest_col = data),
+#      rich_genus = taxa_rich(.dataframe = .,
+#                             .key_col = uid,
+#                             .group_col = genus,
+#                             .unnest_col = data),
+#      rich_target_taxon = taxa_rich(.dataframe = .,
+#                                    .key_col = uid,
+#                                    .group_col = target_taxon,
+#                                    .unnest_col = data),
+#      gini_simpson_ept = taxa_div(.dataframe = .,
+#                                  .key_col = uid,
+#                                  .counts_col = total,
+#                                  .group_col = target_taxon,
+#                                  .filter = order %in% c("ephemeroptera",
+#                                                         "plecoptera",
+#                                                         "trichoptera"),
+#                                  .job = "gini_simpson",
+#                                  .unnest_col = data),
+#      simpson_ept = taxa_div(.dataframe = .,
+#                             .key_col = uid,
+#                             .counts_col = total,
+#                             .group_col = target_taxon,
+#                             .filter = order %in% c("ephemeroptera",
+#                                                    "plecoptera",
+#                                                    "trichoptera"),
+#                             .job = "simpson",
+#                             .unnest_col = data),
+#      shannon_ept = taxa_div(.dataframe = .,
+#                             .key_col = uid,
+#                             .counts_col = total,
+#                             .group_col = target_taxon,
+#                             .filter = order %in% c("ephemeroptera",
+#                                                    "plecoptera",
+#                                                    "trichoptera"),
+#                             .job = "shannon",
+#                             .base_log = 2,
+#                             .unnest_col = data),
+#      pct_ept = taxa_pct(.dataframe = .,
+#                         .key_col = uid,
+#                         .counts_col = total,
+#                         .filter = order %in% c("ephemeroptera",
+#                                                "plecoptera",
+#                                                "trichoptera"),
+#                         .unnest_col = data),
+#      pct_cote = taxa_pct(.dataframe = .,
+#                          .key_col = uid,
+#                          .counts_col = total,
+#                          .filter = order %in% c("coleoptera",
+#                                                 "odonata",
+#                                                 "trichoptera",
+#                                                 "ephemeroptera"),
+#                          .unnest_col = data),
+#      dom_1_target_taxon = taxa_dom(.dataframe = .,
+#                                    .key_col = uid,
+#                                    .counts_col = total,
+#                                    .group_col = target_taxon,
+#                                    .dom_level = 1,
+#                                    .unnest_col = data),
+#      dom_5_target_taxon = taxa_dom(.dataframe = .,
+#                                    .key_col = uid,
+#                                    .counts_col = total,
+#                                    .group_col = target_taxon,
+#                                    .dom_level = 5,
+#                                    .unnest_col = data),
+#      tol_index = taxa_tol_index(.dataframe = .,
+#                                 .key_col = uid,
+#                                 .counts_col = total,
+#                                 .tol_col = ptv,
+#                                 .unnest_col = data)
+#    ) %>%
+#    dplyr::bind_cols(
+#      taxa_seq(.dataframe = .,
+#               .key_col = uid,
+#               .counts_col = total,
+#               .filter_cols_vec = c("class", "order", "family"),
+#               .group_col = target_taxon,
+#               .job = "rich",
+#               .unnest_col = data),
+#      taxa_seq(.dataframe = .,
+#               .key_col = uid,
+#               .counts_col = total,
+#               .filter_cols_vec = c("class", "order", "family"),
+#               .group_col = target_taxon,
+#               .job = "pct_rich",
+#               .unnest_col = data),
+#      taxa_seq(.dataframe = .,
+#               .key_col = uid,
+#               .counts_col = total,
+#               .filter_cols_vec = c("class", "order", "family", "genus"),
+#               .job = "pct",
+#               .unnest_col = data),
+#      taxa_seq(.dataframe = .,
+#               .key_col = uid,
+#               .counts_col = total,
+#               .filter_cols_vec = c("class", "order", "family"),
+#               .group_col = target_taxon,
+#               .job = "simpson",
+#               .unnest_col = data),
+#    )
 
